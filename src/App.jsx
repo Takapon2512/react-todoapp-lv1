37ac5c990dbde3e5bdee13146b3466e721ab2e30
yuki yamagata <yshanxing4@gmail.com>
2023-03-06 10:02:22 +0900

    レビューコメント追記

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ import { Footer } from "./components/Footer";
 export const App = () => {
     const [todoText, setTodoText] = useState('')
     const [editingTodoText, setEditingTodoText] = useState('')
-    const [notstartedTodos, setNotStartedTodos] = useState([])
+    const [notstartedTodos, setNotStartedTodos] = useState([]);
+     // 細かいところですがnot started todos で区切れるとおもうので変数名はnotStartedTodosのように した方がGoodかもです！！
     const [progressTodos, setProgressTodos] = useState([])
     const [endTodos, setEndTodos] = useState([])
 
     //インプットボックスにテキストを入れたときに発動
     const onChangeTodoText = (event) => {
         setTodoText(event.target.value)
+          // ↑一行で書くと const onChangeTodoText = event => setTodoText(event.target.value)のようにかけて2行分短くなってなお良いかもしれません
+          // 大規模なコードになればなるほどこのような細かいところでの行の節約が、「塵も積もればで」効いてきます！！
     }
 
     //Enterを押下することで、追加ボタンを押したときと同じ処理を行う
@@ -23,6 +26,40 @@ export const App = () => {
         if (event.key === 'Enter') {
             onClickAdd()
         }
+
+        // Enterを押したときinputの中が空にならなかったのでよくみてみると
+        // 文字入力の確定時に1回Enterを押すとそのまま未着手のタスクに追加
+        // されさらにもう一回Enterを押すと未着手のタスクに追加されつつ、
+        // ここでinputの値が空になります。
+
+        // ここからは推測になるのですが、この1度目のIME確定(入力確定)のEnterはあくまで「入力を確定させるだけ」
+        // とブラウザは認識しているため、onClickAddの処理が走り最後にInputを空にする処理を入れてもinputの値が
+        // 空にならないのかなと思っています
+        // 確定した後にinputにフォーカスを当てた状態でEnterを押下するとうまくいくことを根拠に上記のように考えました
+
+        // 従って考えるべきは、 「IMEの入力確定のイベントの時はハンドリンクぜず、確定後のEnterのみ
+        // ハンドリングする」といったような感じになります。
+        // ここら辺少し難しいですね....
+
+        // 下記に解決策を記述しますので、余裕があれば実際に解決策1 or 解決策2のコメントアウトをはずして試してみてください！
+        // 解決策1 KeyboardEvent.isComposing を使用する
+        // https://developer.mozilla.org/ja/docs/Web/API/KeyboardEvent/isComposing(引用: MDN)
+        // isComposingでは文字の変換中はtrueを返し、そうでない場合はfalseを返すみたいです
+        // if (!event.nativeEvent.isComposing && event.key === “Enter”) {
+        //     onClickAdd()
+        // }
+        // ただこちらはブラウザによって挙動が違うようで、Safariの場合IME確定時のEnterを押した際に、isComposing
+        // が本来trueになってほしいところ、falseになってしまうそうです。
+
+
+        // 解決策2 keyCodeを使用する
+        // 普通のEnterはkeyCodeが13に、IME確定のEnterは229になります
+        // KeyCodeの使用はJSが公式で非推奨としているのですが、ブラウザの差異を考えなくても良いので
+        // もしかすると解決策2の方がおすすめかもです...
+        // if (event.keyCode === 13) {
+        //     onClickAdd()
+        // }
+
     }
 
     //編集モードのとき、インプットボックスのテキストが変化したときに発動
@@ -46,6 +83,11 @@ export const App = () => {
     //追加ボタンをクリックしたときに発動
     const onClickAdd = () => {
         //もし、インプットボックスのテキストが空でないなら
+        // <- ここのバリデーションめっちゃ良いです！！このような考慮ができるのは素晴らしいです！！
+
+        // カスタムの課題です。
+        // もし入力欄が空の場合、ユーザーに入力欄がからであることを通知し
+        // それ以降の処理が走らないような記述をしてみてください！
         if (todoText !== '') {
             const newTodos = [...notstartedTodos, {
                 id: notstartedTodos.length + 1,
@@ -81,9 +123,11 @@ export const App = () => {
 
         setProgressTodos(newProgressTodos)
         setNotStartedTodos(newNotStartedJobTodos)
+
+
     }
 
-    //削除ボタンをクリックしたときに発動
+    // 削除ボタンをクリックしたときに発動
     const onClickDeleteNotStarted = (index) => {
         const newNotStartedTodos = [...notstartedTodos]
 
@@ -93,6 +137,29 @@ export const App = () => {
         idRewrite(newNotStartedTodos)
         setNotStartedTodos(newNotStartedTodos)
     }
+
+    // 追加時の処理、削除時の処理どちらとも非常によくできています！！
+    // たかぽんさんのコードで基本的に全く問題ないですが、一応私が考えたコードも下記に追記しておきます
+    // 参考程度に見てみてください！
+
+    //  ============================ 追記 wrote by Yuuki ============================================
+    // const onClickJob = (index) => {
+    //     const newNotStartedJobTodos = createNewTodosArray(notstartedTodos, index);
+    //     setProgressTodos([...progressTodos, {  id: progressTodos.length + 1,  title: notstartedTodos[index].title }])
+    //     setNotStartedTodos(newNotStartedJobTodos);
+    // }
+
+    // const onClickDeleteNotStarted = (index) => {
+    //     const newNotStartedTodos = createNewTodosArray(notstartedTodos, index);
+    //     setNotStartedTodos(newNotStartedTodos);
+    // }
+    
+    // const createNewTodosArray = (todosArray, index) => {
+    //     return todosArray.filter(notStartedTodos=> notStartedTodos.id !== notstartedTodos[index].id)
+    //                       .map((notstartedTodos, index) => ( {...notstartedTodos, id: index + 1}) );
+    //  配列のメソッドはメソッドは戻り値が配列になるのでメソッドを繋げることができます。(ちなみにこれをメソッドチェーンと言います)
+    // }
+    //  ============================ 追記ここまで wrote by Yuuki ===============================================
 
     const onClickDeleteProgress = (index) => {
         const newProgressTodos = [...progressTodos]
@@ -159,13 +226,19 @@ export const App = () => {
 
     }
 
+    // 戻す時の処理もidの欠番、重複を考慮できており素晴らしいです!!
+    // 上記の処理で問題ないと思います！！
+
     //idのふり直し処理を関数化
+    // この共通化も素晴らしいです！！！
     const idRewrite = (Todos) => {
         for (let i = 0; i < Todos.length; i++) {
             Todos[i].id = i + 1
         }   
     }
 
+
+    // コンポーネント化が綺麗にできていて素晴らしいです！！かなりコードの見通しが良いです！！
     return (
         <>
         <Header />
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+
+
+// export const InputTodo = ({ todoText, onChange, onKeyDown, onClick }) => {
+//     // 引数で受け取った時点で分割代入する上記のような記述でもいいかもしれません！
+//     // やっていることはたかぽんさんのコードと同じです！
+
 export const InputTodo = (props) => {
     const { todoText, onChange, onKeyDown, onClick } = props
 
